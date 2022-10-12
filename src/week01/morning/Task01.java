a1f77e8c82e23be23f9af831bbf88b2e2a3265a0
OscarSah <oscar@cybertekschool.com>
2022-10-12 18:29:22 +0300

    Task01 Started

---
+++ src/week01/morning/Task01.java
@@ -0,0 +1,23 @@
+package week01.morning;
+
+public class Task01 {
+    public static void main(String[] args) {
+
+/*
+Declare and Assign value to Variables
+DataType variableName = VALUE;
+DataType variableName1, variableName2 ;
+variableName1 = VALUE;
+variableName2 = VALUE;
+Write a Java program that accepts two integers and then prints the sum, the difference, the product, the average, the distance (the difference between integer), the maximum (the larger of the two integers), the minimum (smaller of the two integers).
+ */
+        int firstInt = 25;
+        int secondInt = 5;
+        int sum, difference, product, max, min;
+        double average;
+
+        // arithmetic operators
+        sum = firstInt + secondInt;
+        System.out.println("sum = " + sum);
+    }
+}
