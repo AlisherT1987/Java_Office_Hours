3a9bf66d58807cee4d2656bd84b102e2046244a5
AdamB25 <barsialtunit@gmail.com>
2022-10-19 21:35:06 -0400

    Week4 T5_Animal

---
+++ src/week04/evening/T5_Animal.java
@@ -0,0 +1,24 @@
+package week04.evening;
+
+public class T5_Animal {
+
+
+
+    /*
+    Task 5:
+
+Create  a program that accepts animal as String
+
+							DOG 	- domestic animal
+							CAT 	- domestic animal
+							TIGER 	- wild  animal
+
+							For other animal - unknown animal
+
+							INPUT : DOG 		EXPECTED OUTPUT : DOG is domestic animal
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
