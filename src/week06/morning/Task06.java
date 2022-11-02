b38cb70d2453c7e402e3e48e972b4a48f98b18de
OscarSah <oscar@cybertekschool.com>
2022-11-02 20:19:24 +0300

    Task06 for week06

---
+++ src/week06/morning/Task06.java
@@ -0,0 +1,17 @@
+package week06.morning;
+
+public class Task06 {
+    public static void main(String[] args) {
+        System.out.println(in1020(12,35));
+    }
+
+    public static boolean in1020(int n1, int n2){
+        boolean result = false;
+        // logic  for either of one
+        if( ( 10 <= n1 && n1 <= 20) || ( 10 <= n2 && n2 <= 20) ){
+            result = true;
+        }
+
+        return result;
+    }
+}
