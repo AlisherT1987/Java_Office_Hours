89f9bc6aa96aaf6d292552d00509d26c6a3b1246
AdamB25 <barsialtunit@gmail.com>
2022-11-02 21:34:06 -0400

    Week6

---
+++ src/week06/evening/Task3_RemoveSpaces.java
@@ -0,0 +1,21 @@
+package week06.evening;
+
+public class Task3_RemoveSpaces {
+
+    /*
+    Task 3 :    Write a method that can remove  all extra space from String , trim() is forbidden
+
+// create a loop --- taking every index one by one with charAt(i)  and compares against ' '   str.charAt(i) == ' '
+don't add empty to result, if not empty add it to result string
+
+    			 Input: "  Hello "
+        	     Output: Hello
+     */
+
+
+    public static void main(String[] args) {
+removeSpaces("  Hello ");
+
+
+    }
+}
