b099a2fa55bad28526d1432677149caada56ca95
AdamB25 <barsialtunit@gmail.com>
2022-11-02 21:43:08 -0400

    Week6

--- src/week06/evening/Task3_RemoveSpaces.java
+++ src/week06/evening/Task3_RemoveSpaces.java
@@ -14,8 +14,25 @@ don't add empty to result, if not empty add it to result string
 
 
     public static void main(String[] args) {
-removeSpaces("  Hello ");
+        ourTrim("  Hello ");
 
+
+    }
+
+    private static void ourTrim(String str) {
+        String result="";
+        for (int i = 0; i <str.length() ; i++) {
+            char ch = str.charAt(i);
+           /* if (ch!=' ')
+                result+=ch;
+
+            */
+
+            if (ch==' ')
+                continue;
+            result+=ch;
+        }
+        System.out.println("result = " + result);
 
     }
 }
