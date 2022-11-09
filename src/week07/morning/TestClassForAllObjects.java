d280f10cb4b7d99cbbdf76e7b863d76f2b306f9e
OscarSah <oscar@cybertekschool.com>
2022-11-09 21:57:50 +0300

    Gas Tank

--- src/week07/morning/TestClassForAllObjects.java
+++ src/week07/morning/TestClassForAllObjects.java
@@ -41,5 +41,23 @@ public class TestClassForAllObjects {
         System.out.println("str.length() = " + str.length());
         System.out.println("str.charAt(0) = " + str.charAt(0));
         System.out.println("calculator2.multiply() = " + calculator2.multiply());
+        
+        
+        // GasTank Tests
+        GasTank gasTank = new GasTank();
+        gasTank.setInfo(100,60);
+        System.out.println("gasTank.amount = " + gasTank.amount);
+
+        //gasTank.useGas(70);
+        //System.out.println("gasTank.amount = " + gasTank.amount);
+        while (!gasTank.isEmpty()){
+            gasTank.useGas(0.1);
+        }
+        System.out.println("gasTank.getGasLevel() = " + gasTank.getGasLevel());
+
+       // System.out.println("How much you need in Turkey to fill up " + (gasTank.fillUp()*25));
+        // minimum wage 5.500 TL fill up around 4 times
+        System.out.println("How much you need in USA to fill up " + (gasTank.fillUp()*0.90));
+        // minimum wage in the USA : 2500 Dollars  fill up 50 times
     }
 }
