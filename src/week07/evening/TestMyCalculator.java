77c459ccce7993b5b3f60f064d85512a742a5138
AdamB25 <barsialtunit@gmail.com>
2022-11-10 04:48:55 +0300

    Week 7

---
+++ src/week07/evening/TestMyCalculator.java
@@ -0,0 +1,14 @@
+package week07.evening;
+
+public class TestMyCalculator {
+
+    public static void main(String[] args) {
+
+        MyCalculator myCalculator=new MyCalculator();
+
+        System.out.println(myCalculator.plus(2, 3));
+
+        System.out.println(myCalculator.multiply(4, 5));
+
+    }
+}
