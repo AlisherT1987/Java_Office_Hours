8bdbf47bcab3f72c9ce5fd8b2a3baa0b42391359
AdamB25 <barsialtunit@gmail.com>
2022-11-17 04:53:09 +0300

    Week 8

--- src/week08/evening/ArraysIntro.java
+++ src/week08/evening/ArraysIntro.java
@@ -47,5 +47,76 @@ public class ArraysIntro {
 
         }
 
+        //===================================================
+        //print the elements
+
+
+        for (String weekDay : weekDays) {
+            System.out.println(weekDay);
+        }
+                   //0123
+        String name="Adam";
+
+
+        //print the letter chars one by one
+
+        for (int i = 0; i < name.length(); i++) {
+            System.out.println(name.charAt(i));
+        }
+
+        char[] chars = name.toCharArray();// char[] chars={'A','d','a','m'}
+
+        for (char eachCharacter : chars) {
+            System.out.println(eachCharacter);
+        }
+        //iter short cut for foreach loop
+        for (char aChar : chars) {
+
+        }
+        //===================================================
+
+        // if you want to create 2 dimensional array we need to use 2 [][]
+
+        int [][] TwoDimension={
+
+                {1,2,3},  // first 1 Dimension array // index=0
+              // 0 1 2
+                {4,5,6},  // second 1 Dimension array //index=1
+              // 0 1 2
+                {7,8,9}  //third 1 Dimension array   //index=2
+
+
+        };
+        System.out.println("=======================================");
+        // get the number 5
+
+        System.out.println(TwoDimension[1][1]);
+
+        // get the number 8
+        System.out.println(TwoDimension[2][1]);
+                            //3
+
+        for (int i = 0; i < TwoDimension.length; i++) { // first loop for getting each ARRAY
+
+            for (int j = 0; j < TwoDimension[i].length; j++) { // second loop for getting each  ELEMENT
+                System.out.println(TwoDimension[i][j]);
+            }
+
+        }
+
+
+        for (int[] EachDimensionalArray : TwoDimension) { // the first for getting each array
+            for (int eachElement : EachDimensionalArray) { // th second one for getting element
+                System.out.println(eachElement);
+            }
+
+        }
+
+
+
+
+
+
+
     }
 }
