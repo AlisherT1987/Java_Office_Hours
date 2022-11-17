53c65a3b9509f63a140d26a918f73e181df88c74
AdamB25 <barsialtunit@gmail.com>
2022-11-17 05:42:54 +0300

    Week 8

--- src/week08/evening/Task2_MultipleWords.java
+++ src/week08/evening/Task2_MultipleWords.java
@@ -15,9 +15,15 @@ public class Task2_MultipleWords {
 
     public static void main(String[] args) {
 
+String str="knife, wooden spoons, plates, cups, forks, pan, pot, trash can, fridge, dish washer";
 
+String [] words=str.split(", "); // we need to separate words by using delimeter
 
+        for (String eachWord : words) {
+            if (eachWord.contains(" ")) // if my word contains space it means that word is multiple
+                System.out.println(eachWord);
 
+        }
 
     }
 }
