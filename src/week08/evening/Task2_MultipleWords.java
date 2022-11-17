83f38438a1232d39644668bc20858f5da405eb8e
AdamB25 <barsialtunit@gmail.com>
2022-11-17 05:36:59 +0300

    Week 8

---
+++ src/week08/evening/Task2_MultipleWords.java
@@ -0,0 +1,23 @@
+package week08.evening;
+
+public class Task2_MultipleWords {
+
+    /*
+    Task 2 :    Write a program that accepts string and prints multiple words in the string
+
+        "knife, wooden spoons, plates, cups, forks, pan, pot, trash can, fridge, dish washer"
+
+        output : wooden spoons
+                 trash can
+                 dish washer
+
+     */
+
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
