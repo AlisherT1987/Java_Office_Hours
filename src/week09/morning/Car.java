92065e48181270a1e4da992acc1ed735b6d5d86f
OscarSah <oscar@cybertekschool.com>
2022-11-23 20:55:46 +0300

    Store Objects to Arraylist

---
+++ src/week09/morning/Car.java
@@ -0,0 +1,28 @@
+package week09.morning;
+
+public class Car {
+
+    public String make;
+    public String model;
+    public int year;
+    public double price;
+    public String color;
+
+    public void setInfo(String make, String model, int year, double price, String color) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+    }
+
+    public String toString() {
+        return "Car{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
---
+++ src/week09/morning/Task04.java
@@ -0,0 +1,13 @@
+package week09.morning;
+
+public class Task04 {
+    public static void main(String[] args) {
+        /*
+        Write a Java program to insert an element into the array list at the first position. Then retrieve an element (at a specified index) from the array list. Then update specific array element by given element. Lastly remove the third element from the array list.
+         */
+
+        // create 5 different objects of Car class and store them inside an Arraylist, then loop through each of them to find which one has max price????????
+
+
+    }
+}
