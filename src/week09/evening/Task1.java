f2b14b9cef9fa4e09abb4d97511d98de51bb77e0
AdamB25 <barsialtunit@gmail.com>
2022-11-24 04:36:03 +0300

    Week 9 Task 2

--- src/week09/evening/Task1.java
+++ src/week09/evening/Task1.java
@@ -15,10 +15,16 @@ int[] z = {1,3,5} = > 0
         // creating array if we know the elements
         int[] x = {2, 1, 2, 3, 4};
 
+        int[] y = {2,2,0};
+        int[] z = {1,3,5};
+
         //if we don't know the elements only knowing number of element
         //   int[] y=new  int[10];
 
         int result = findNumberOfEvenNumbersInArray(x);
+        int result1 = findNumberOfEvenNumbersInArray(y);
+        int result2 = findNumberOfEvenNumbersInArray(z);
+        System.out.println(result+result1+result2);
         //for win alt+Enter For mac option+enter
         System.out.println("result = " + result);
         System.out.println(result);
