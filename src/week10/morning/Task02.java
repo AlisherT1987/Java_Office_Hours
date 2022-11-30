9fba9860e7480e973a6a18eaef6b78a50bf0f027
OscarSah <oscar@cybertekschool.com>
2022-11-30 20:11:47 +0300

    Task02 for week 10

--- src/week10/morning/Task02.java
+++ src/week10/morning/Task02.java
@@ -19,10 +19,24 @@ output:
         System.out.println("merge(list1,list2) = " + Arrays.toString(merge(list1, list2)));
     }
 // short solution, however you could create loops and add them
-    public static Integer[] merge(ArrayList<Integer> list1, ArrayList<Integer> list2){
-            list1.addAll(list2);  // merged arraylists
-            Integer[] result = list1.toArray(new Integer[0]);
-            return result;
+//    public static Integer[] merge(ArrayList<Integer> list1, ArrayList<Integer> list2){
+//            list1.addAll(list2);  // merged arraylists
+//            Integer[] result = list1.toArray(new Integer[0]);
+//            return result;
+//    }
+public static Integer[] merge(ArrayList<Integer> list1, ArrayList<Integer> list2){
+    Integer[] result = new Integer[list1.size()+list2.size()];
+    int index = 0;
+    for (Integer integer : list1) {
+        result[index] = integer; // 0,1,2
+        index++;  // post increament : 1,2,3 : exit loop
     }
+    // System.out.println("index = " + index); // 3
+    for (Integer integer : list2) {
+        result[index] = integer;
+        index++;
+    }
+    return result;
+}
 
 }
