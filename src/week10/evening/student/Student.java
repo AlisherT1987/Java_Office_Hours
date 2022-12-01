0b855a68be8d276d4e3ba2feb4d3fc9bab7c4c5c
AdamB25 <barsialtunit@gmail.com>
2022-12-01 05:08:15 +0300

    Week 10 Task 4

---
+++ src/week10/evening/student/Student.java
@@ -0,0 +1,60 @@
+package week10.evening.student;
+
+public class Student {
+    /*
+
+Task 4:
+
+ Class Name: Student
+
+			instance variables:
+					name, age, batchNumber
+
+			static variables :
+					school
+
+			1st constructor: initializes the name ONLY
+
+			2nd constructor: initializes name & age
+						(MUST use constructor call to set the name)
+
+			3rd Constructor: initializes name, age ,batchNumber
+						(MUST use constructor call to set the name & age)
+
+
+			instance methods: toString()
+
+     */
+    public String name;
+    public int age, batchNumber;
+
+    public static String school="Cydeo";
+
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public Student(String name, int age) {
+        this(name);
+        this.age = age;
+    }
+
+    public Student(int batchNumber,String name) {
+        this(name);
+        this.batchNumber=batchNumber;
+    }
+
+    public Student(String name, int age, int batchNumber) {
+        this(name, age);
+        this.batchNumber = batchNumber;
+    }
+
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", batchNumber=" + batchNumber +
+                '}';
+    }
+}
