6f29d40565e0a476aa170058e2832bd1906f5ab7
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2022-12-14 19:55:28 +0300

    week 12 morning is added

---
+++ src/week12/morning/BankAccount.java
@@ -0,0 +1,26 @@
+package week12.morning;
+
+public class BankAccount {
+
+    private String name = "Jack";
+    private double balance = 1000;
+
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
---
+++ src/week12/morning/EncapsulationReview.java
@@ -0,0 +1,22 @@
+package week12.morning;
+
+public class EncapsulationReview {
+
+    public static void main(String[] args) {
+
+        BankAccount bankAccount = new BankAccount();
+
+        System.out.println(bankAccount.getName());
+        System.out.println(bankAccount.getBalance());
+
+        bankAccount.setName("Watson");
+        System.out.println(bankAccount.getName());
+
+        bankAccount.setBalance(1000 + 10000);
+        System.out.println(bankAccount.getBalance());
+
+
+
+
+    }
+}
