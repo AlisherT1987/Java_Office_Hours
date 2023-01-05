7cb774d038e34a878c80de97c64395d91765897a
AdamB25 <barsialtunit@gmail.com>
2023-01-05 05:15:05 +0300

    Week 15 task2 is done

--- src/week15/evening/collection/Task2.java
+++ src/week15/evening/collection/Task2.java
@@ -1,5 +1,7 @@
 package week15.evening.collection;
 
+import java.util.*;
+
 public class Task2 {
     /*
 
@@ -13,6 +15,20 @@ public class Task2 {
       ahmet ,mehmet,rafet,saffet,temel
      */
 
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>(Arrays.asList("ahmet", "rafet", "temel", "mehmet", "saffet"));
+        System.out.println("unsorted names : "+ names);
+        List<String> sorted = sortedList(names);
+        System.out.println("sorted names: "+sorted);
+
+    }
+
+    private static List<String> sortedList(List<String> names) {
+        Set<String> sorted=new TreeSet<>(names);
+
+     return    new ArrayList<String>(sorted);
+
+    }
 
 
 }
