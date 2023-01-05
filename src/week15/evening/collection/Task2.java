f1bd84161e1ecd38d2448a2121e9c772f241707e
AdamB25 <barsialtunit@gmail.com>
2023-01-05 04:54:02 +0300

    Week 15 collections Task1

---
+++ src/week15/evening/collection/Task2.java
@@ -0,0 +1,18 @@
+package week15.evening.collection;
+
+public class Task2 {
+    /*
+
+  --    create a list put the names
+         ahmet
+         rafet
+         mehmet
+         saffet
+         temel
+     -- create a method and return the sorted list
+      ahmet ,mehmet,rafet,saffet,temel
+     */
+
+
+
+}
