e6f25ca0ca0784fe4a3c11df1227d53751b5823b
AdamB25 <barsialtunit@gmail.com>
2023-01-12 04:29:05 +0300

    Week 16 task2 is ready

--- src/week16/evening/Spartans.java
+++ src/week16/evening/Spartans.java
@@ -10,7 +10,6 @@ public class Spartans {
         spartan1.put("name","Adam");
         spartan1.put("gender","Male");
         spartan1.put("phone",1234567890);
-        System.out.println(spartan1);
 
 
         Map<String, Object> spartan2 =new HashMap<>();
@@ -18,14 +17,14 @@ public class Spartans {
         spartan2.put("name","Aynur");
         spartan2.put("gender","Female");
         spartan2.put("phone",1234567890);
-        System.out.println(spartan2);
+
 
         Map<String, Object> spartan3=new HashMap<>();
         spartan3.put("id",3);
         spartan3.put("name","Emre");
         spartan3.put("gender","Male");
         spartan3.put("phone",1234567890);
-        System.out.println(spartan3);
+
 
 
         Map<String, Object> spartan4 =new HashMap<>();
@@ -33,28 +32,28 @@ public class Spartans {
         spartan4.put("name","Nigar");
         spartan4.put("gender","Female");
         spartan4.put("phone",1234567890);
-        System.out.println(spartan4);
+
 
         Map<String, Object> spartan5 =new HashMap<>();
         spartan5.put("id",5);
         spartan5.put("name","Fazilat");
         spartan5.put("gender","Female");
         spartan5.put("phone",1234567890);
-        System.out.println(spartan5);
+
 
         Map<String, Object> spartan6 =new HashMap<>();
         spartan6.put("id",6);
         spartan6.put("name","Alev");
         spartan6.put("gender","Female");
         spartan6.put("phone",1234567890);
-        System.out.println(spartan6);
+
 
         Map<String, Object> spartan7 =new HashMap<>();
         spartan7.put("id",7);
         spartan7.put("name","Melda");
         spartan7.put("gender","Female");
         spartan7.put("phone",1234567890);
-        System.out.println(spartan7);
+
 
 
         Map<String, Object> spartan8 =new HashMap<>();
@@ -62,7 +61,7 @@ public class Spartans {
         spartan8.put("name","Hamid");
         spartan8.put("gender","Male");
         spartan8.put("phone",1234567890);
-        System.out.println(spartan8);
+
 
 
         Map<String, Object> spartan9 =new HashMap<>();
@@ -70,7 +69,7 @@ public class Spartans {
         spartan9.put("name","Vasyl");
         spartan9.put("gender","Male");
         spartan9.put("phone",1234567890);
-        System.out.println(spartan9);
+
 
 
         Map<String, Object> spartan10 =new HashMap<>();
@@ -78,19 +77,18 @@ public class Spartans {
         spartan10.put("name","Kubra");
         spartan10.put("gender","Female");
         spartan10.put("phone",1234567890);
-        System.out.println(spartan10);
+
 
         Map<String, Object> spartan11 =new HashMap<>();
         spartan11.put("id",11);
         spartan11.put("name","Serhan");
         spartan11.put("gender","Male");
         spartan11.put("phone",1234567890);
-        System.out.println(spartan11);
+
 
         //Spartan
         List<Map<String,Object>> spartans=new ArrayList<>();
         spartans.addAll(Arrays.asList(spartan2,spartan3,spartan4,spartan5,spartan6,spartan11,spartan1,spartan7,  spartan8,spartan9,spartan10));
-        System.out.println(spartans.size());
 return spartans;
     }
 }
